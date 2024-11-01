03a33c6c33cfc80222b518139d16f4bccc293473
Johnathan Dunker <djohnathan@wustl.edu>
2024-11-01 10:38:09 -0500

    opt_att_1

--- fir11/src/fir11_kernel.cpp
+++ fir11/src/fir11_kernel.cpp
@@ -18,20 +18,23 @@ extern "C" {
     coef_t c[N] = {53, 0, -91, 0, 313, 500, 313, 0, -91, 0, 53};
 
     static data_t shift_reg[N];
+    #pragma HLS ARRAY_PARTITION variable = shift_reg complete dim = 0
     acc_t acc;
     int i;
 
     acc = 0;
-    Shift_Accum_Loop:
-      for(i = N - 1; i >= 0; i--) {
-        if (i == 0) {
-          acc += x * c[0];
-          shift_reg[0] = x;
-        } else {
-          shift_reg[i] = shift_reg[i - 1];
-          acc += shift_reg[i] * c[i];
-        }
-        *y = acc;
+    TDL:
+      for(i = N - 1; i > 0; i--)
+      {
+        #pragma HLS PIPELINE II = 1
+        shift_reg[i] = shift_reg[i-1];
       }
+      shift_reg[0] = x;
+    MAC:
+      for(i = N - 1; i >= 0; i--) {
+        #pragma HLS PIPELINE II = 1
+        acc += shift_reg[i] * c[i];
+      }
+    *y = acc;
   }
 }
